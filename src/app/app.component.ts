f4cb17cb279895f5eca7baad3adb839d4706400a
rabbanimd <dev.rabbani@gmail.com>
2021-11-23 00:39:07 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+declare var SockJS : any;
+declare var Stomp : any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chat-app';
+
+  greetings : string [] = [];
+
+  disabled = true;
+
+  newmessage = '';
+
+  private stompClient : any;
+  
+  constructor(){
+  }
+
+  ngOnInit()
+  {
+    this.connect();
+  }
+
+  setConnected(connected:boolean)
+  {
+    this.disabled = !connected;
+    if(connected)
+    {
+      this.greetings = [];
+    }
+  }
+
+  connect(){
+    const socketUrl =  'http://localhost:8080/testchat';
+    const socket = new SockJS(socketUrl);
+    this.stompClient = Stomp.over(socket);
+    const _this = this;
+    this.stompClient.connect({}, function (frame: any){
+      console.log("connected "+frame);
+      _this.stompClient.subscribe('/start/initial', function (hello:any) {
+        console.log(JSON.parse(hello.body));
+        _this.showMessage(JSON.parse(hello.body));
+      });
+    });
+  }
+
+
+  sendMessage() {
+    this.stompClient.send(
+      '/current/resume',
+      {},
+      JSON.stringify(this.newmessage)
+    );
+    this.newmessage = ""; 
+  }
+
+  showMessage(message:any) {
+    this.greetings.push(message);
+  }
 }
